359f227fb8cfbf728d43e1f012db65f0f69a400c
wiroon.sri@WSYMBAM1 <wiroon.sri@innosoft.kmutt.ac.th>
2022-08-20 13:41:15 +0700

    step 7

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
-      home: RandomWords(),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+      ),
+      home: const RandomWords(),
     );
   }
 }
