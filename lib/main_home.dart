2c272da76b18104826586ccbeb5693ca7d268bf4
Crowzy <puchit.aro@innosoft.kmutt.ac.th>
2022-09-03 22:07:18 +0700

    เพิ่มฟังก์ชั่นสำหรับรับค่าจาก noti และส่งไป dialog

--- lib/main_home.dart
+++ lib/main_home.dart
@@ -2,6 +2,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_101/my_dialog.dart';
 
 class MainHome extends StatefulWidget {
   const MainHome({Key? key}) : super(key: key);
@@ -22,6 +23,22 @@ class _MainHomeState extends State<MainHome> {
       FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
       String? token = await firebaseMessaging.getToken();
       print('token ==> $token');
+
+      //ตอนใช้งาน App อยู่
+      FirebaseMessaging.onMessage.listen((event) {
+        String? title = event.notification!.title;
+        String? body = event.notification!.body;
+        print('Noti ตอนเปิด App title ==> $title, body ==> $body');
+        MyDialog(context: context).normalDialog(title: title!, body: body!);
+      });
+
+      //ตอนย่อ App
+      FirebaseMessaging.onMessageOpenedApp.listen((event) {
+        String? title = event.notification!.title;
+        String? body = event.notification!.body;
+        print('Noti ตอนย่อ App title ==> $title, body ==> $body');
+        MyDialog(context: context).normalDialog(title: title!, body: body!);
+      });
     });
   }
 
